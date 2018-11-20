ff3a19d26cd756415dd87605685389614ae3ae62
w058ff <w058ff@yamaguchi-u.ac.jp>
2018-11-21 02:14:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/enpit_p12/sainttropez/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.enpit_p12.sainttropez
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.enpit_p12.sainttropez.R.id.button1
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onMainButtonTapped(view: View?){
+        val intent = Intent(this, buyctivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return  true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.top -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.log -> {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.sell -> {
+                val intent = Intent(this, SellActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.history  -> {
+                val intent = Intent(this, HistoryActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+}
+
---
+++ app/src/main/java/com/example/enpit_p12/sainttropez/SellActivity.kt
@@ -0,0 +1,44 @@
+package com.example.enpit_p12.sainttropez
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class SellActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sell)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return  true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.top -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.log -> {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.sell -> {
+                val intent = Intent(this, SellActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.history -> {
+                val intent = Intent(this, HistoryActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+}
